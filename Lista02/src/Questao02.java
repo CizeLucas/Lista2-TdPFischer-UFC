951e1fb477a01e6880c7ff5ead5b72f1ae019095
CizeLucas <cizelucas@gmail.com>
2023-03-25 14:31:46 -0300

    update 1

--- Lista02/src/Questao02.java
+++ Lista02/src/Questao02.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class Questao02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int inputNum=0;
+		int sumNum=0;
+		
+		System.out.println("Digite os Numeros:");
+		do {
+			sumNum = sumNum + inputNum;
+			inputNum = Integer.parseInt(scanner.nextLine());
+		}while(inputNum>=0);
+		
+		System.out.printf("A soma dos numeros positivos digitados foi: %d", sumNum);
 
+		scanner.close();
 	}
 
 }
--- Lista02/src/Questao05.java
+++ Lista02/src/Questao05.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Questao05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
