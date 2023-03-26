656f38d0abda7b91fda48497c33ec576c2f49a2d
CizeLucas <cizelucas@gmail.com>
2023-03-26 14:31:18 -0300

    update2

--- Lista02/src/Questao07.java
+++ Lista02/src/Questao07.java
@@ -1,9 +1,25 @@
-
+import java.util.Scanner;
+import java.lang.Math;
 public class Questao07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		//   1   2   3   4   5   6   7
+		
+		int numPrimo=2;		int i; 
+		
+		for(numPrimo=2; numPrimo<=1000 ;numPrimo++) {
+			
+			if(numPrimo%2!=0 || numPrimo==2)
+				if(numPrimo%3!=0 || numPrimo==3)
+					if(numPrimo%5!=0 || numPrimo==5)
+						if(numPrimo%7!=0 || numPrimo==7)
+							if(numPrimo%11!=0 || numPrimo==11)
+								System.out.print(numPrimo + "  ");
+			
+		
+			
+		}
 	}
 
 }
--- Lista02/src/Questao08.java
+++ Lista02/src/Questao08.java
@@ -1,9 +1,51 @@
+import java.util.ArrayList;
 
 public class Questao08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		boolean isPrimo = true;
+		int numLimite = 1000;
+		
+		ArrayList<Integer> numCheck = new ArrayList<Integer>();
+		ArrayList<Integer> numPrimos = new ArrayList<Integer>();
+		
+		for(int i = 2; i*i<=numLimite; i++) {
+			numCheck.add(i);
+		}
+		
+		//System.out.println(numCheck.toString());
+		
+		for(int i=0; i<numCheck.size(); i++) {
+			//System.out.println(numCheck.toString());
+		
+			for(int j=0; j<numCheck.size(); j++) {
+				if(numCheck.get(j)%numCheck.get(i)==0 && numCheck.get(j)!=numCheck.get(i)) {
+					//System.out.printf("%d pct %d = %d   ", numCheck.get(j), numCheck.get(i), numCheck.get(j)%numCheck.get(i) );
+					//System.out.println("Removendo: "+numCheck.get(j));
+					numCheck.remove(j);
+				}
+			}
+			
+		}
+		
+		System.out.println(numCheck.toString());
+			
+		for(int i=2; i<=numLimite; i++)
+			numPrimos.add(i);
+		
+		for(int i=0;  i<numCheck.size(); i++) {
+			for(int j=0; j<numPrimos.size(); j++){
+				if(numPrimos.get(j)%numCheck.get(i)==0 && numPrimos.get(j)!=numCheck.get(i)) {
+					numPrimos.remove(j);
+				}
+			}
+		}
+		
+		for(int i=0; i<numPrimos.size(); i++) {
+			System.out.print(numPrimos.get(i)+ " ");
+		}
+		//System.out.println(numPrimos.size());
 	}
 
 }
