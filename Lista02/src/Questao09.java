23dcaea1359cbf2ec58bdf979cb1fdc77d8471e8
CizeLucas <cizelucas@gmail.com>
2023-03-30 22:21:46 -0300

    update 6

--- Lista02/src/Questao09.java
+++ Lista02/src/Questao09.java
@@ -1,27 +1,59 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Questao09 {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("Digite quantas letras voce quer inserir: ");
-		int numChars = Integer.parseInt(scan.nextLine());
-		
-		char inputChar[] = new char[numChars];
-		
-		for(int i=0; i<numChars; i++) {
-			inputChar[i] = scan.nextLine().toUpperCase().charAt(0);
-		}
-		int numCharsFact = numChars;
-		
-		for(int i=numChars; i>1; i--) {
-			numCharsFact = numCharsFact * (i-1);
-		}
-		System.out.println(numCharsFact);
+	    public static void permute(int[] vector, int start, List<int[]> permutations) {
+	        if (start == vector.length - 1) {
+	            permutations.add(vector.clone());
+	        } else {
+	            for (int i = start; i < vector.length; i++) {
+	                swap(vector, start, i);
+	                permute(vector, start + 1, permutations);
+	                swap(vector, start, i);
+	            }
+	        }
+	    }
+	    
+	    public static void swap(int[] vector, int i, int j) {
+	        int temp = vector[i];
+	        vector[i] = vector[j];
+	        vector[j] = temp;
+	    }
+	    
+	    public static void main(String[] args) {
+	    	Scanner scan = new Scanner(System.in);
+	    	System.out.print("Digite quantas letras voce quer inserir: ");
+			int numChars = Integer.parseInt(scan.nextLine());
+			char inputChar[] = new char[numChars];
+			int[] vector = new int[numChars];
+			
+			for(int i=0; i<numChars; i++) {
+				System.out.print("Digite um caractere: ");
+				inputChar[i] = scan.nextLine().toUpperCase().charAt(0);
+			}
+			
+			for(int i=0; i<numChars; i++) {
+				vector[i]=i;
+			}
+
+	        List<int[]> permutations = new ArrayList<>();
+	        permute(vector, 0, permutations);
+	        
+	        System.out.println();
+	        for (int[] permutation : permutations) {
+	            for(int i=0; i<permutation.length; i++) {
+	            	System.out.print(inputChar[permutation[i]]);
+	            }
+	            System.out.println();
+	        }
+	        
+	        scan.close();
+	    }
 
 		
-//PERMUTACAO:  0 1 2	0 2 1	1 0 2	1 2 0	2 0 1	2 1 0		
 		
-	}
-}	
+}
+
+
